522cb1de5c050574d0943288418bff348f2b8ae6
davidVeer <davidvdveer05@gmail.com>
2025-10-04 19:34:12 +0200

    removing the sending of messages

--- Client_PI_Activiteit_Challenge/Messenger.cs
+++ Client_PI_Activiteit_Challenge/Messenger.cs
@@ -12,48 +12,12 @@ namespace Showcase_Client_PI_Activiteit
     {
         public static void SendChatMessage(string rawOutgoingChatMessage, NetworkStream clientNetworkStream)
         {
-            string outgoingChatMessage;
-            string commandRecognissionRegexString = @"^\/.*[\s|:]";
-            Regex commandRecognissionRegex = new Regex(commandRecognissionRegexString);
-
-            FormsCommands.ShowMessageInChatbox("You:" + rawOutgoingChatMessage);
-
-            if (commandRecognissionRegex.IsMatch(rawOutgoingChatMessage))
-            {
-                outgoingChatMessage = FindServerCommand(rawOutgoingChatMessage);
-            }
-            else {
-                outgoingChatMessage = "101:" + rawOutgoingChatMessage;
-            }
-
-            if (clientNetworkStream != null && clientNetworkStream.CanWrite)
-            {
-                byte[] data = Encoding.UTF8.GetBytes(outgoingChatMessage);
-                clientNetworkStream.Write(data, 0, data.Length);
-            }
+            
         }
 
         public static void SendInitializingMessage(string newClientName, NetworkStream clientNetworkStream)
         {
-            if (clientNetworkStream != null && clientNetworkStream.CanWrite)
-            {
-                byte[] data = Encoding.UTF8.GetBytes("111:" + newClientName);
-                clientNetworkStream.Write(data, 0, data.Length);
-            }
-        }
 
-        private static string FindServerCommand(string potentialCommand) {
-            string outgoingMessage;
-
-            switch (potentialCommand) {
-                case { } when potentialCommand.StartsWith("/whisper:"):
-                    string messageWithoutCommand = potentialCommand.Substring(potentialCommand.IndexOf(":")+1);
-                    outgoingMessage = "102:" + messageWithoutCommand;
-                    return outgoingMessage;
-                default:
-                    outgoingMessage = "121:" + potentialCommand;
-                    return outgoingMessage;    
-            }
         }
     }
 }
