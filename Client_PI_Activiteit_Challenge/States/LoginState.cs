415167ff9a4674d01803710a3a039afe38603cb0
davidVeer <davidvdveer05@gmail.com>
2025-10-04 20:00:13 +0200

    State comments added

--- Client_PI_Activiteit_Challenge/States/LoginState.cs
+++ Client_PI_Activiteit_Challenge/States/LoginState.cs
@@ -11,7 +11,13 @@ namespace Showcase_Client_PI_Activiteit.States
     {
         public override void HandleMessage(string incommingServerMessage)
         {
-
+            /* Dit is deel van een Bonus challenge, 
+             * hij is goed te doen maar niet vereist (zie ConnectionService.cs -> ReadMessages() )
+             * 
+             * Zorg dat de berichten die hier worden afgehandeld beginnen met een "211:" protocol. 
+             * Als dit niet zo is wordt er niets gedaan, als dit wel zo is kun je de chatroom openen, 
+             * het bericht laten zien in de chatbox en de client state veranderen naar de volgende state
+             */
         }
     }
 }
